cda9ba67138b39a35f9098c0748b3a8de4758c82
lizeyliz <alizaeaton@gmail.com>
2025-03-14 08:22:51 -0700

    adding vertex class

--- Vertex.java
+++ Vertex.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+
 public class Vertex {
-    
+    //instance variables
+    private String data; //data associated with a given vertex
+    private ArrayList<Edge> edges; //arraylist to hold all edges
+
+    //constructor
+    public Vertex(String inputData) {
+        this.data = inputData;
+        this.edges = new ArrayList<Edge>();
+    }
+
+    //takes in end vertex and weight
+    public void addEdge(Vertex endVertex, Integer weight) {
+        this.edges.add(new Edge(this, endVertex, weight));
+    }
+
+    public void removeEdge(Vertex endVertex) {
+        this.edges.removeIf(edge -> edge.getEnd().equals(endVertex));
+    }
+
+    public ArrayList<Edge> getEdges(){
+        return this.edges;
+    }
 }
